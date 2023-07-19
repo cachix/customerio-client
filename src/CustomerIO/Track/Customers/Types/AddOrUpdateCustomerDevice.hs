86b351d5abd7d8a4b473cbb0f284eb1734815ed1
Sander <hey@sandydoo.me>
2023-07-19 21:12:31 +0000

    Add the customer id bodies

---
+++ src/CustomerIO/Track/Customers/Types/AddOrUpdateCustomerDevice.hs
@@ -0,0 +1,50 @@
+module CustomerIO.Track.Customers.Types.AddOrUpdateCustomerDevice where
+
+import Data.Aeson
+import CustomerIO.Aeson
+
+data AddOrUpdateCustomerDeviceBody = MkAddOrUpdateCustomerDeviceBody
+  { aucdDeviceId :: Text
+  , aucdPlatform :: Text
+  , aucdLastUsed :: Maybe Timestamp
+  , aucdAttributes :: Maybe DeviceAttributes
+  }
+
+instance ToJSON AddOrUpdateCustomerDevice where
+  toJSON MkAddOrUpdateCustomerDeviceBody {..} =
+    object [ mkPair "device" device]
+    where
+      device = mkObject
+        [ mkPair "id" <$> Just aucdDeviceId
+        , mkPair "platform" <$> Just aucdPlatform
+        , mkPair "last_used" <$> aucdLastUsed
+        , mkPair "attributes" <$> aucdAttributes
+        ]
+
+data DeviceAttributes = MkDeviceAttributes
+  { daDeviceOs :: Maybe Text
+  , daDeviceModel :: Maybe Text
+  , daAppVersion :: Maybe Text
+  , daCioSdkVersion :: Maybe Text
+  , daDeviceLocale :: Maybe Text
+  , daPushEnabled :: Maybe Bool
+  , daExtraDeviceAttributes :: Maybe Object
+  }
+
+instance ToJSON DeviceAttributes where
+  toJSON MkDeviceAttributes {..} = case daExtraDeviceAttributes of
+    Just km -> Object (mainFields <> km)
+    Nothing -> Object mainFields
+    where
+      mainFields = mkObject
+        [ mkPair "device_os" <$> daDeviceOs
+        , mkPair "device_model" <$> daDeviceModel
+        , mkPair "app_version" <$> daAppVersion
+        , mkPair "cio_sdk_version" <$> daCioSdkVersion
+        , mkPair "device_locale" <$> daDeviceLocale
+        , mkPair "push_enabled" . boolToText <$> daPushEnabled
+        ]
+
+boolToText :: Bool -> Text
+boolToText True = "true"
+boolToText False = "false"
