b1ace27387073ceb1bce87c38e7bdd03faf9fd7e
Sander <hey@sandydoo.me>
2023-07-19 23:36:28 +0000

    Fix customer types

--- src/CustomerIO/Track/Customers/Types/AddOrUpdateCustomer.hs
+++ src/CustomerIO/Track/Customers/Types/AddOrUpdateCustomer.hs
@@ -2,7 +2,7 @@ module CustomerIO.Track.Customers.Types.AddOrUpdateCustomer
   ( module CustomerIO.Track.Customers.Types.AddOrUpdateCustomer
   ) where
 
-import CustomerIO.Aeson (defaultAesonOptions)
+import CustomerIO.Aeson (defaultAesonOptions, mkObject, mkPair)
 import CustomerIO.Track.Events.Types.Core (Timestamp)
 import Data.Aeson (Object, ToJSON(toJSON), Value(..))
 import Data.Aeson.TH (deriveToJSON)
@@ -13,7 +13,7 @@ data AddOrUpdateCustomerBody = MkAddOrUpdateCustomerBody
   , aucEmail                      :: Maybe Text
   , aucAnonymousId                :: Maybe Text
   , aucCreatedAt                  :: Maybe Timestamp
-  , aucUpdate                     :: Maybe Bool
+  , auc_Update                    :: Maybe Bool
   , aucCioRelationships           :: Maybe CioRelationships
   , aucUnsubscribed               :: Maybe Bool
   , aucCioSubscriptionPreferences :: Maybe Object -- TODO
@@ -32,13 +32,28 @@ data CioRelationships = MkCioRelationships
   , crRelationships :: [Identifier]
   }
 
+deriveToJSON defaultAesonOptions ''Identifier
+deriveToJSON defaultAesonOptions ''CioRelationships
+
 instance ToJSON CioRelationshipAction where
   toJSON AddCioRelationships = String "add_relationships"
   toJSON RemoveCioRelationships = String "remove_relationships"
 
+instance ToJSON AddOrUpdateCustomerBody where
+  toJSON MkAddOrUpdateCustomerBody {..} = case aucAttributes of
+    Just km -> Object (mainFields <> km)
+    Nothing -> Object mainFields
+    where
+      mainFields = mkObject
+        [ mkPair "id" <$> aucId
+        , mkPair "email" <$> aucEmail
+        , mkPair "anonymous_id" <$> aucAnonymousId
+        , mkPair "created_at" <$> aucCreatedAt
+        , mkPair "_update" <$> auc_Update
+        , mkPair "cio_relationships" <$> aucCioRelationships
+        , mkPair "unsubscribed" <$> aucUnsubscribed
+        , mkPair "cio_subscription_preferences" <$> aucCioSubscriptionPreferences
+        ]
+
 defaultAddOrUpdateCustomerBody :: AddOrUpdateCustomerBody
 defaultAddOrUpdateCustomerBody = MkAddOrUpdateCustomerBody Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing Nothing
-
-deriveToJSON defaultAesonOptions ''Identifier
-deriveToJSON defaultAesonOptions ''CioRelationships
-deriveToJSON defaultAesonOptions ''AddOrUpdateCustomerBody
